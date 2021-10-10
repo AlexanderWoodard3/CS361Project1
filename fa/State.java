fb60697e55a92e140f4bbb40d5ca731c58b009ae
Alexander Woodard <alexanderwoodard@u.boisestate.edu>
2021-10-10 17:17:20 -0600

    fa finished

--- fa/State.java
+++ fa/State.java
@@ -2,6 +2,19 @@ package fa;
 
 public abstract class State {
 
-    
+    /**
+	 * The state label.
+	 * It should be a unique name.
+	 * @author elenasherman
+	 */
+	protected String name;
+	
+	/**
+	 * getter for the string label
+	 * @return returns the state label.
+	 */
+	public String toString(){
+		return name;
+	}
 
 }
