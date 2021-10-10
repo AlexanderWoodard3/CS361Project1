229afb9f0a60a9855d8f7947a05eefee401ccd67
Alexander Woodard <alexanderwoodard@u.boisestate.edu>
2021-10-10 17:45:51 -0600

    DFAState.java complete. DFA.java incomplete

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -1,1 +1,112 @@
+package fa.dfa;
+import fa.dfa.DFAInterface;
+import fa.FAInterface;
+import fa.State;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+/**
+ * 
+ * Description needs to be written
+ * 
+ * @author Alexander Woodard and Quinn Shultz
+ */
+public class DFA implements DFAInterface, FAInterface {
+
+    private LinkedHashSet<DFAState> Q;
+	private LinkedHashSet<Character> sigma;
+	private HashMap<String, DFAState> delta;
+	private DFAState q0;
+	private LinkedHashSet<DFAState> F;
+
+    /* Description needs to be written */
+    public DFA() {
+		Q = new LinkedHashSet<DFAState>();
+		sigma = new LinkedHashSet<Character>();
+		delta = new HashMap<String, DFAState>();
+		q0 = null;
+		F = new LinkedHashSet<DFAState>();
+	}
+
+    @Override
+    public void addStartState(String name) {
+
+    }
+
+    @Override
+    public void addState(String name) {
+
+    }
+
+    @Override
+    public void addFinalState(String name) {
+
+    }
+
+    @Override
+    public void addTransition(String fromState, char symbol, String toState) {
+
+    }
+
+    @Override
+	public Set<? extends State> getStates() {
+		return;
+	}
+
+	@Override
+	public Set<? extends State> getFinalStates() {
+		return;
+	}
+
+	@Override
+	public State getStartState() {
+		return;
+	}
+
+	@Override
+	public Set<Character> getABC() {
+		return;
+	}
+
+    @Override
+    public boolean accepts(String s) {
+        return;
+    }
+
+    @Override
+    public DFAState getToState(DFAState from, char onSymb) {
+        return;
+    }
+
+    @Override
+    public String toString() {
+        return;
+    }
+
+    /* Description needs to be written */
+    private String getQ() {
+        return;
+    }
+
+    /* Description needs to be written */
+    private String getSigma() {
+        return;
+    }
+
+    /* Description needs to be written */
+    private String getDelta() {
+        return;
+    }
+
+    /* Description needs to be written */
+    private String getQ0() {
+        return;
+    }
+
+    /* Description needs to be written */
+    private String getF() {
+        return;
+    }
+
+}
--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -1,1 +1,26 @@
+package fa.dfa;
+import fa.State;
 
+/**
+ * creates a state for a DFA
+ * The state object has a name.
+ * 
+ * @author Alexander Woodard and Quinn Shultz
+ *
+ */
+public class DFAState extends State {
+	
+    private String name;
+	
+	/**
+	 * Creates a state that has a unique name.
+	 * @param name - the name of the state
+	 */
+	public DFAState(String name ) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return name;
+	}	
+}
